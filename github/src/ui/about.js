3f1de57394ab9f90b288cede223537aec0de97e9
Stefanie Habersatter <habi1986@gmx.at>
2017-03-21 18:02:09 +0100

    Configuring about

--- github/src/ui/about.js
+++ github/src/ui/about.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { observer, inject } from "mobx-react";
 
+function Authenticated(Component) {
+    return inject("repoStore", "sessionStore")(
+        observer(function({sessionStore}) {
+            if (sessionStore.authenticated) {
+                return <Component/>;
+            } else {
+                return null;
+            }
+        })
+    );
+}
 
-export default inject("repoStore", "sessionStore")(
+export default Authenticated(
+    inject("sessionStore")(
     observer(function({sessionStore}) {
-        if (sessionStore.authenticated) {
-            const currentUser = (window.currentUser = sessionStore.currentUser);
-
-            return (
-                <div>
-                    <h3>User: {currentUser.login}</h3>
-                    <p>Mail: {currentUser.email}</p>
-                    <p>Followers: {currentUser.followers}</p>
-                </div>
-            );
-        } else {
-            return null;
-        }
-
-
+    const currentUser = (window.currentUser = sessionStore.currentUser);
+        return (
+            <div>
+                <h3>User: {currentUser.login}</h3>
+                <p>Mail: {currentUser.email}</p>
+                <p>Followers: {currentUser.followers}</p>
+            </div>
+        );
     })
+    )
 );
